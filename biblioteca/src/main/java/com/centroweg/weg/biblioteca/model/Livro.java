2ffa7df00204d29ae2603b9d65e1250e35d43f4d
Kaua Felix <kfelixcosta11@gmail.com>
2026-02-19 14:42:24 -0300

    feat: adiciona metodos getters e setters que esqueci

--- biblioteca/src/main/java/com/centroweg/weg/biblioteca/model/Livro.java
+++ biblioteca/src/main/java/com/centroweg/weg/biblioteca/model/Livro.java
@@ -22,4 +22,36 @@ public class Livro {
 
     public Livro() {
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
 }
