ac8d492f05e20910676259752fa2ae6a8daf56a5
Kaua Felix <kfelixcosta11@gmail.com>
2026-02-19 18:36:41 -0300

    feat: implementa CRUD para a entidade Emprestimo na classe EmprestimoController

--- biblioteca/src/main/java/com/centroweg/weg/biblioteca/controller/emprestimo/EmprestimoController.java
+++ biblioteca/src/main/java/com/centroweg/weg/biblioteca/controller/emprestimo/EmprestimoController.java
@@ -1,4 +1,86 @@
 package com.centroweg.weg.biblioteca.controller.emprestimo;
 
+import com.centroweg.weg.biblioteca.model.Emprestimo;
+import com.centroweg.weg.biblioteca.service.emprestimo.EmprestimoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/emprestimo")
 public class EmprestimoController {
+
+    private EmprestimoService service;
+
+    public EmprestimoController(EmprestimoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Emprestimo save (@RequestBody Emprestimo emprestimo){
+        try{
+            return service.salvar(emprestimo);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping
+    public List<Emprestimo> findAll(){
+        try{
+            return service.listar();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public Emprestimo findById (@PathVariable int id) {
+        try {
+            return service.buscarPorId(id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/{usuarioId}/usuario")
+    public List<Emprestimo> listarPorUsuario(@PathVariable int usuarioId) {
+        try {
+            return service.listarPorUsuario(usuarioId);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao listar os empréstimos do usuário com ID: " + usuarioId + " || " + e.getMessage());
+        }
+
+    }
+
+    @PutMapping("/{id}")
+    public Emprestimo updateEmprestimo(@PathVariable int id, @RequestBody Emprestimo emprestimo){
+        try{
+            return service.atualizar(emprestimo, id);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao atualizar o empréstimo com ID: "+id+ " || "+e.getMessage());
+        }
+    }
+
+    @PutMapping ("/devolucao/{id}")
+    public void devolver(@PathVariable int id, @RequestBody Emprestimo emprestimo){
+        try{
+            service.devolver(id, emprestimo.getData_devolucao());
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao devolver o empréstimo com ID: "+id+ " || "+e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id){
+        try{
+            service.deletar(id);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao atualizar o empréstimo com ID: "+id+ " || "+e.getMessage());
+        }
+    }
 }
